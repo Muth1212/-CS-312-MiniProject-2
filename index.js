f9102bcc9fcb2026ea0f9c372f3f0be7bf04c481
Muth1212 <156949366+Muth1212@users.noreply.github.com>
2024-09-30 16:30:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-require('dotenv').config(); // Load .env file
+require('dotenv').config(); 
 const express = require('express');
 const app = express();
 const homeRoute = require('./routes/home');
 const port = process.env.PORT || 3000;
 
-// Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
-// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
 
-// Use the home route
 app.use('/', homeRoute);
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
